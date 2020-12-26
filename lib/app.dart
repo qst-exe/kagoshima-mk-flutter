335e16fc4b701a236d987eeeac8e03564d5c2a6e
Task Nagashige <task.19.xx@gmail.com>
2020-12-26 17:57:35 +0900

    [Fix] ページのフォルダ階層の修正

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'ui/pages/home.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '鹿児島.mk 投票アプリ',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+      // onGenerateRoute: PageRouter.generate,
+    );
+  }
+}
